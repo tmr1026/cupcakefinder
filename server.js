eea479beae9836d1245a0410e920a16b64bf330e
Taylor Reese <taylor@Taylors-MBP.attlocal.net>
2019-05-08 15:23:40 -0400

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var app = express();
-var PORT = process.env.PORT||3000;
+var PORT = process.env.PORT || 8080;
 //import routes
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
